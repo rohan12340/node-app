046a9eef5bb3d8c3e90b0656d95a4090ec3c0893
unknown <rohanmugale45@gmail.com>
2026-06-17 13:22:48 +0530

    new version 7

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -4,15 +4,11 @@ set -e
 
 echo "Logging into ECR..."
 
-aws ecr get-login-password 
---region ap-south-1 | docker login 
---username AWS 
---password-stdin 276375887088.dkr.ecr.ap-south-1.amazonaws.com
+aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 276375887088.dkr.ecr.ap-south-1.amazonaws.com
 
 echo "Stopping old container..."
 
 docker stop node-app || true
-
 docker rm node-app || true
 
 echo "Pulling latest image..."
@@ -21,10 +17,10 @@ docker pull 276375887088.dkr.ecr.ap-south-1.amazonaws.com/node-app:latest
 
 echo "Starting new container..."
 
-docker run -d 
---name node-app 
--p 3000:3000 
---restart always 
-276375887088.dkr.ecr.ap-south-1.amazonaws.com/node-app:latest
+docker run -d \
+  --name node-app \
+  -p 3000:3000 \
+  --restart always \
+  276375887088.dkr.ecr.ap-south-1.amazonaws.com/node-app:latest
 
 echo "Deployment completed successfully"
